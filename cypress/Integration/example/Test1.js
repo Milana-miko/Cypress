78c7f2b793f8b424cb07f921791d3d70a381157e
Milana <milanamiko88@gmail.com>
2023-11-17 22:01:18 +0400

    Added initial stucture of the project

---
+++ cypress/Integration/example/Test1.js
@@ -0,0 +1,10 @@
+describe("My Cool First Test Suite", function() {
+    it("my first Test case", function() {
+        cy.visit("https://yerevan-city.am/");
+    });
+/*
+    it("my second Test case", function() {
+        
+    });
+    */
+})
---
+++ cypress/Integration/example/Test2.js
@@ -0,0 +1,5 @@
+describe("Testing SAS", function() {
+    it("Test case 1", function() {
+        cy.visit("https://www.sas.am/");
+    });
+})
